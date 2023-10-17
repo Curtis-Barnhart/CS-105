b745e79b0b7d456a70119d9e23b92c1d1c64e862
curtis <cbarnhart@westmont.edu>
2023-10-17 15:09:23 -0700

    libpng now links on curtis' machine

--- main.cpp
+++ main.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
 
+#include "png.h"
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+//    std::cout << "Hello, World!" << std::endl;
+//    return 0;
+
+    fprintf(stderr, "   Compiled with libpng %s; using libpng %s.\n",
+            PNG_LIBPNG_VER_STRING, png_libpng_ver);
+//    fprintf(stderr, "   Compiled with zlib %s; using zlib %s.\n",
+//            ZLIB_VERSION, zlib_version);
     return 0;
 }
