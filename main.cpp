53c7b4f68b47ef063897ed94876dce3c301e3abe
curtis <cbarnhart@westmont.edu>
2023-11-06 15:32:03 -0800

    reseting repo so we can follow new tutorial

--- main.cpp
+++ main.cpp
@@ -1,14 +1,6 @@
 #include <iostream>
 
-#include "png.h"
-
 int main() {
-//    std::cout << "Hello, World!" << std::endl;
-//    return 0;
-
-    fprintf(stderr, "   Compiled with libpng %s; using libpng %s.\n",
-            PNG_LIBPNG_VER_STRING, png_libpng_ver);
-//    fprintf(stderr, "   Compiled with zlib %s; using zlib %s.\n",
-//            ZLIB_VERSION, zlib_version);
+    std::cout << "Hello, World!" << std::endl;
     return 0;
 }
