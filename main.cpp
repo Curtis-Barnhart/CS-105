bc7a3bb50be9020763ef1482acfac3487e263864
jnielsen713 <jnielsen@westmont.edu>
2023-11-06 15:44:39 -0800

    set up main to write a range of pixels with blue and green values to an image it is redirected to

--- main.cpp
+++ main.cpp
@@ -1,6 +1,28 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    // Image
+
+    int image_width = 256;
+    int image_height = 256;
+
+    //Render
+
+    std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+
+    for (int j = 0; j < image_height; j++)
+    {
+        for (int i = 0; i < image_width; i++)
+        {
+            auto r = 0;
+            auto g = double(j) / (image_width - 1);
+            auto b = double(i) / (image_width - 1);
+
+            int ir = static_cast<int>(255.999 * r);
+            int ig = static_cast<int>(255.999 * g);
+            int ib = static_cast<int>(255.999 * b);
+
+            std::cout << ir << ' ' << ig << ' ' << ib << '\n';
+        }
+    }
 }
