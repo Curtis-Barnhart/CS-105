1c24f12d29f4573b87dba40b868419e67c6ff030
Meghan Nugent <mnugent@westmont.edu>
2023-11-06 17:14:21 -0800

    added a progress indicator

--- main.cpp
+++ main.cpp
@@ -12,6 +12,7 @@ int main() {
 
     for (int j = 0; j < image_height; j++)
     {
+        std::clog <<"\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
         for (int i = 0; i < image_width; i++)
         {
             auto r = 0;
@@ -25,4 +26,6 @@ int main() {
             std::cout << ir << ' ' << ig << ' ' << ib << '\n';
         }
     }
+
+    std::clog << "\rDone.                       \n";
 }
