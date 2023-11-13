48122cd8a777a235c007f5286e6adac111b89f53
Meghan Nugent <mnugent@westmont.edu>
2023-11-12 20:08:29 -0800

    finished 5.2

--- main.cpp
+++ main.cpp
@@ -4,8 +4,19 @@
 
 #include <iostream>
 
+bool hit_sphere(const Point3& center, double radius, const Ray& r){
+    Vec3 oc = r.origin() - center;
+    auto a = dot(r.direction(), r.direction());
+    auto b = 2.0 * dot(oc, r.direction());
+    auto c = dot(oc, oc) - radius*radius;
+    auto discriminant = b*b - 4*a*c;
+    return (discriminant >= 0);
+}
+
 Color ray_color(const Ray& r)
 {
+    if (hit_sphere(Point3(0,0,-1), 0.5, r))
+        return Color(1, 0, 0);
     Vec3 unit_direction = unit_vector(r.direction());
     auto a = 0.5*(unit_direction.y() + 1.0);
     return (1.0 - a)*Color(1.0, 1.0, 1.0) + a*Color(0.5, 0.7, 1.0);
