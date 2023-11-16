f8477c98576b72703425dfe7593681d4ca876275
curtis <cbarnhart@westmont.edu>
2023-11-15 17:20:14 -0800

    finished 6.2, used math to simplify code. Yay math

--- main.cpp
+++ main.cpp
@@ -15,11 +15,11 @@
  */
 double hit_sphere(const Point3& center, double radius, const Ray& r) {
     Vec3 oc = r.origin() - center;
-    auto a = dot(r.direction(), r.direction());
-    auto b = 2.0 * dot(oc, r.direction());
-    auto c = dot(oc, oc) - radius*radius;
-    auto discriminant = b*b - 4*a*c;
-    return (discriminant < 0) ? -1.0 : (-b - std::sqrt(discriminant)) / (2.0*a);
+    auto a = r.direction().length_squared();
+    auto half_b = dot(oc, r.direction());
+    auto c = oc.length_squared() - radius*radius;
+    auto discriminant = half_b*half_b - a*c;
+    return (discriminant < 0) ? -1.0 : (-half_b - std::sqrt(discriminant)) / a;
 }
 
 /**
