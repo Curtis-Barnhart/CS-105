d91ca3336836985f3203b9380423d3d5ebb88d32
jnielsen713 <jnielsen@westmont.edu>
2023-12-05 11:15:35 -0800

    section 8.1 random number functions

--- RTproject.h
+++ RTproject.h
@@ -2,8 +2,10 @@
 #define RTPROJECT_H
 
 #include <cmath>
+#include <cstdlib>
 #include <limits>
 #include <memory>
+#include <random>
 
 //#include "Interval.h"
 //#include "Ray.h"
@@ -24,6 +26,16 @@ const double pi = 3.1415926535897932385;
 
 inline double degrees_to_radians(double degrees) {
     return degrees * pi / 180.0;
+}
+
+inline double random_double() {
+    static std::uniform_real_distribution<double> distribution(0.0, 1.0);
+    static std::mt19937 generator;
+    return distribution(generator);
+}
+
+inline double random_double(double min, double max) {
+    return min + (max - min) * random_double();
 }
 
 // common headers
